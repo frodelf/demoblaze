f3b389ffeaac525b5c7fe56927ae0405890bb3e6
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-10-08 19:09:00 +0300

    Initial commit

---
+++ src/main/java/com/demoblaze/DemoblazeApplication.java
@@ -0,0 +1,13 @@
+package com.demoblaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoblazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoblazeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoblaze/DemoblazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoblaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoblazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
